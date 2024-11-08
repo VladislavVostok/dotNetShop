40e7b20b7a28971263e5bcc60b4c50cec10a05dc
Vladislav East <vladislav.sage@yandex.ru>
2024-11-08 18:49:23 +0300

    Работа с переменными окружения и Secret Manager

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 using dotNetShop.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.DataProtection;
+using dotNetShop.Services;
+using System.Text;
 
 
 // dotnet tool install --global dotnet-ef    	Установка dotnet-ef инструменты для миграции
@@ -13,21 +16,60 @@ namespace dotNetShop
 		// Shalom
 		public static void Main(string[] args)
 		{
-			var builder = WebApplication.CreateBuilder(args);
+			var builder = WebApplication.CreateBuilder();
 
-			builder.Services.AddDbContext<ShopDBContext>(options => options.UseMySQL(builder.Configuration.GetConnectionString("MySqlConnectionString")));
+
+			string connectionString = builder.Configuration["ProdConnectionString"];
+			builder.Services.AddDbContext<ShopDBContext>(options => options.UseMySQL(connectionString));
+
+
 
 			// Add services to the container.
 			builder.Services.AddControllersWithViews();
 
+			#region
+			//Secret Manager
+			//builder.Configuration.AddUserSecrets<Program>();
+			//string connectionString = builder.Configuration["DevConnectionString"];
+			//builder.Services.AddDbContext<ShopDBContext>(options => options.UseMySQL(connectionString));
+
+
+			//Переменные окружения
+			//builder.Configuration.AddEnvironmentVariables();
+			//string connectionString = builder.Configuration["ProdConnectionString"];
+			//builder.Services.AddDbContext<ShopDBContext>(options => options.UseMySQL(connectionString));
+
+
+			// Шифрование конфигурационного файла
+			//builder.Services.AddDataProtection()
+			//	.PersistKeysToFileSystem(new DirectoryInfo(@"C:\Users\Студент\Desktop\dotnetshop\very_secret_dont_worry")) // Укажите путь для хранения ключей
+			//	.SetApplicationName("MyUniqueApp"); // Уникальное имя приложения
+
+			//builder.Services.AddSingleton<AppSettingsEncryptionService>();
+			#endregion
+
 			var app = builder.Build();
+
+			#region
+			//var appSettingsEncryptionService = app.Services.GetRequiredService<AppSettingsEncryptionService>();
+			//EncryptAppSettings(appSettingsEncryptionService);
+
+
+			// Загрузка зашифрованного файла
+			//var encryptedJson = File.ReadAllText("appsettings.json");
+			//var decryptedJson = appSettingsEncryptionService.Decrypt(encryptedJson);
+
+			//builder.Configuration.AddJsonStream(new MemoryStream(Encoding.UTF8.GetBytes(decryptedJson)));
+
+			#endregion
+			
+
 
 			// Configure the HTTP request pipeline.
 			if (!app.Environment.IsDevelopment())
 			{
 				app.UseExceptionHandler("/Home/Error");
 			}
-
 
 			app.UseStaticFiles();
 
@@ -40,6 +82,22 @@ namespace dotNetShop
 				pattern: "{controller=Home}/{action=Index}/{id?}");
 
 			app.Run();
+		}
+
+
+		static void EncryptAppSettings(AppSettingsEncryptionService encryptionService)
+		{
+			// Путь к файлу appsettings.json
+			var filePath = "appsettings.json";
+
+			// Чтение содержимого файла
+			var json = File.ReadAllText(filePath);
+
+			// Шифрование содержимого
+			var encryptedJson = encryptionService.Encrypt(json);
+
+			// Запись зашифрованного содержимого обратно в файл
+			File.WriteAllText(filePath, encryptedJson);
 		}
 	}
 }
