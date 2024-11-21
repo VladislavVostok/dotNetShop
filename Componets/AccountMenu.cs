e15ff5eef2a5964ef2ef18a0590aee2789785abf
Vladislav East <vladislav.sage@yandex.ru>
2024-11-21 20:14:14 +0300

    Добавление компонента на хидер меню для вывода элемента логин если пользователь не авторизован или выйти из аккаунта.

--- Componets/AccountMenu.cs
+++ Componets/AccountMenu.cs
@@ -2,13 +2,35 @@
 
 namespace dotNetShop.Componets
 {
-    public class AccountMenu: ViewComponent
+    public class AccountMenuViewComponent : ViewComponent
     {
         // TODO: Разобраться с компонентами 
 		// https://csharp.webdelphi.ru/komponenty-predstavlenij-v-asp-net-core-mvc-viewcomponentresult-i-rabota-s-predstavleniyami/
 		public IViewComponentResult Invoke()
         {
-            return View("Default",HttpContext.User);
+            return View("AccountMenu", HttpContext.User);
+        }
+    }
+
+
+    public class UsersListViewComponent : ViewComponent
+    {
+        List<string> users = new()
+        {
+            "Tom", "Tim", "Bob", "Sam", "Alice", "Kate"
+        };
+        public IViewComponentResult Invoke()
+        {
+            int number = users.Count;
+            // если передан параметр number
+            if (Request.Query.ContainsKey("number"))
+            {
+                int.TryParse(Request.Query["number"], out number);
+            }
+
+            ViewBag.Users = users.Take(number);
+            ViewData["Header"] = $"Количество пользователей: {number}.";
+            return View();
         }
     }
 }
